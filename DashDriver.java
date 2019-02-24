bff64756ad39638bcde5ab432f65cac70d888bc2
Yusuf Mostafa <Electrex@users.noreply.github.com>
2019-02-24 12:12:39 -0800

    Add Starter code for Driver class
    Not finished, add stuff later.

---
+++ DashDriver.java
@@ -0,0 +1,38 @@
+//Driver class for program
+
+import java.io.File;
+import java.util.Scanner;
+
+public class MovieDatabase {
+    public static void main(String[] args) throws Exception{
+        int numBuckets = 26; //Choose whatever
+        //Hash<Product> p = new Hash<Product>(numBuckets);
+        String userChoice = "";
+        String name;
+        String productID;
+        double cost;
+        double unitPrice;
+        Scanner reader = new Scanner(System.in);
+        File file = new File("data.txt"); // or .xml
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine()){
+            name = sc.nextLine();
+            productID = sc.nextLine();
+            cost = sc.nextDouble();
+            sc.nextLine();
+            unitPrice = sc.nextDouble();
+            if (sc.hasNextLine()){
+                sc.nextLine();
+                sc.nextLine();
+            }
+            p.insert(new Product(unitPrice, name, productID, cost));
+        }
+        System.out.println("Welcome to Dash!\n");
+
+        do {
+            //Do stuff
+        } while ();
+        System.out.println("Goodbye!");
+    }
+}
