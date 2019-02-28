a11e0eff7b4d2d27f6fda4d054a9e4a4bc70666d
Albert Liu <albertliu20010714@gmail.com>
2019-02-27 16:53:25 -0800

    Albert's part
    IO package
    UI package
    Employees

---
+++ IO/SettleIO.java
@@ -0,0 +1,13 @@
+package IO;
+
+public class SettleIO {
+	private String filename, fileExtention, fileType;
+	
+	public SettleIO(String fname, String fextention, String filetype)
+	{
+		
+	}	
+	
+	
+	
+}
