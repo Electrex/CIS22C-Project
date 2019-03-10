243678d21af2c383701ae0c801ecb9e67a46356c
Albert Liu <albertliu20010714@gmail.com>
2019-03-10 10:33:08 -0700

    Albert's push

--- IO/EmployeeIO.java
+++ IO/EmployeeIO.java
@@ -1,5 +1,16 @@
 package IO;
 
 public class EmployeeIO {
-
+	
+	private String filename; 
+	
+	public void readfile()
+	{
+		
+	}
+	
+	public void writefile()
+	{
+		
+	}
 }
---
+++ User/Client.java
@@ -0,0 +1,24 @@
+package User;
+
+public class Client {
+	
+	private ArrayList<Customer> customerlist; 
+	
+	public Client()
+	{
+		
+	}
+	
+	
+	public static void makeOrder()
+	{
+		
+	}
+	
+	public static boolean verifyLogInInformation(String username, String password)
+	{
+		
+	}
+	
+	public static void createnewaccount()
+}
