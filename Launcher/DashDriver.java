7897231e242f5bbed43cd8fc5bcec96464b7bb89
Albert Liu <albertliu20010714@gmail.com>
2019-03-24 22:22:20 -0700

    All IO fixed

--- Launcher/DashDriver.java
+++ Launcher/DashDriver.java
@@ -40,8 +40,8 @@ public class DashDriver {
 	
     public static void main(String[] args) throws Exception{
         
-         debugger();
-    	//runner();
+        //debugger();
+    	runner();
     	
     	
     	/*
